c173fb6691e45a3a0f9bf1331dfd90e3911fd14b
小城 <xiaocheng@xiaochengdeMacBook-Pro.local>
2022-05-10 22:02:19 +0800

    fix: 优化代码

--- index.js
+++ index.js
@@ -5,82 +5,64 @@ const puppeteer = require("puppeteer");
 
 const app = express();
 const domain = "https://www.kgvod.com";
+const options = {
+  //当为true时，客户端不会打开，使用无头模式；为false时，可打开浏览器界面
+  headless: true,
+  args: [
+    "--no-sandbox", // 沙盒模式
+    "--disable-setuid-sandbox", // uid沙盒
+    "--disable-dev-shm-usage", // 创建临时文件共享内存
+    "--disable-accelerated-2d-canvas", // canvas渲染
+    "--disable-gpu" // GPU硬件加速
+  ]
+};
 app.get("/list", async (req, res) => {
   const searchValue = req.query.wd;
   const url = `${domain}/vod-search.html`;
-  const result = await axios({
-    url,
-    method: "get",
-    params: {
-      wd: searchValue
-    }
-  });
-  const list = [];
-  const $ = cheerio.load(result.data);
-  $(".stui-pannel-box .stui-pannel_bd .stui-vodlist__media .active").each(async function () {
-    const title = $(".detail .title a", this).text();
-    const src = $(".detail .title a", this).attr();
-    const pageUrl = (domain + src.href.replace(".html", "") + "-1-1.html").replace(
-      "detail",
-      "play"
-    );
-    // console.log(pageUrl);
-    // const pageContent = $.html(pageUrl);
-    list.push({ title, url: pageUrl });
-  });
-  // for (let i = 0; i < list.length; i++) {
-  //   const url = list[i];
-  //   const user_agent =
-  //     "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36";
-  //   const options = {
-  //     //当为true时，客户端不会打开，使用无头模式；为false时，可打开浏览器界面
-  //     headless: true,
-  //     args: ["--no-sandbox", user_agent]
-  //   };
-  //   try {
-  //     const browser = await puppeteer.launch(options);
-  //     const page = await browser.newPage();
-  //     await page.goto(url);
-  //     const content = await page.content();
-  //     const $$ = cheerio.load(content);
-  //     console.log($$("#playleft iframe").attr().src.split("?m3u8=")[1]);
-  //     page.close();
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
-  // const pageRes = await axios({
-  //   url: "https://www.kgvod.com/vod-play-56554-1-1.html",
-  //   method: "get"
-  // });
-  res.send({ data: list });
+  try {
+    const result = await axios({
+      url,
+      method: "get",
+      params: {
+        wd: searchValue
+      }
+    });
+    const list = [];
+    const $ = cheerio.load(result.data);
+    $(".stui-pannel-box .stui-pannel_bd .stui-vodlist__media .active").each(async function () {
+      const title = $(".detail .title a", this).text();
+      const src = $(".detail .title a", this).attr();
+      const pageUrl = (domain + src.href.replace(".html", "") + "-1-1.html").replace(
+        "detail",
+        "play"
+      );
+      list.push({ title, url: pageUrl });
+    });
+    res.send({ data: list });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/movie", async (req, res) => {
-  const key = req.query.key;
-  // for (let i = 0; i < list.length; i++) {
-  const url = key;
-  const user_agent =
-    "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36";
-  const options = {
-    //当为true时，客户端不会打开，使用无头模式；为false时，可打开浏览器界面
-    headless: true,
-    args: ["--no-sandbox", user_agent]
-  };
-  // try {
+  const url = req.query.key;
+  const result = await axios({
+    url: "https://www.kgvod.com/vod-play-30286-1-1.html",
+    method: "get"
+  });
+  // console.log(result.data);
+  try {
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto("https://www.kgvod.com/vod-play-30286-1-1.html");
     const content = await page.content();
-    const $$ = cheerio.load(content);
-    console.log($$("#playleft iframe").attr().src.split("?m3u8=")[1]);
-    page.close();
-  // } catch (error) {
-    // console.log(error);
-  // }
-  res.send({data: $$("#playleft iframe").attr().src.split("?m3u8=")[1]})
-  // }
+    const $ = cheerio.load(content);
+    console.log($("#playleft iframe").attr().src.split("?m3u8=")[1]);
+    res.send({ data: $("#playleft iframe").attr().src.split("?m3u8=")[1] });
+  } catch (err) {
+    console.log(err);
+  }
+  // page.close();
 });
 
 app.listen(4000, () => {
